f0ea2332b115d575f863ac98f54f405d86075cc2
elfeny7 <ikmalfaris50@gmail.com>
2026-01-22 09:39:16 +0700

    feat: add icon on tabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,14 @@
 import { Tabs } from 'expo-router';
 import { PostProvider } from '@/src/context/PostContext';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <PostProvider>
       <Tabs screenOptions={{ headerTitle: 'Mengposting App' }} >
-        <Tabs.Screen name="index" options={{ title: 'Feed' }} />
-        <Tabs.Screen name="create" options={{ title: 'Create' }} />
-        <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+        <Tabs.Screen name="index" options={{ title: 'Feed', tabBarIcon: ({ color, size }) => (<Ionicons name="home" size={size} color={color} />) }} />
+        <Tabs.Screen name="create" options={{ title: 'Create', tabBarIcon: ({ color, size }) => (<Ionicons name="add-circle" size={size} color={color} />) }} />
+        <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon: ({ color, size }) => (<Ionicons name="person" size={size} color={color} />) }} />
       </Tabs>
     </PostProvider>
   );
